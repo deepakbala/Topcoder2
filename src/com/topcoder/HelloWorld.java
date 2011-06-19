4bb956b5782541fd286569574f5b20a87a2263ca
Deepak Bala <dbalasubramanyam@paypal.com>
2011-06-19 07:33:07 -0700

    initial version

---
+++ src/com/topcoder/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.topcoder;
+
+public class HelloWorld
+{
+
+    public static void main(String... args)
+    {
+        new HelloWorld().go();
+    }
+
+    public void go()
+    {
+        System.out.println("Hello world");
+    }
+}
